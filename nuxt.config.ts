96ae5d4066b500ffcaf86cad4458d425ef03f851
Theekshana Bandara <Theekshana.Bandara@intrepidtravel.com>
2026-05-04 22:31:43 +0530

    link env values

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+
+  // Import automatically from env
+  runtimeConfig: {
+    public: {
+      typesenseHost: "",
+      typesensePort: "",
+      typesenseProtocol: "",
+      typesenseSearchOnlyKey: "",
+    },
+    extends: "./.nuxt/tsconfig.json",
+    include: ["types/**/*.d.ts"],
+  },
+});

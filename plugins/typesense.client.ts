d43f4d7d46b7512c396bc119f32606dfed006b3e
Theekshana Bandara <Theekshana.Bandara@intrepidtravel.com>
2026-05-04 15:02:05 +0530

    create typesense client

---
+++ plugins/typesense.client.ts
@@ -0,0 +1,20 @@
+import Typesense from "typesense";
+import { useRuntimeConfig } from "#imports";
+
+export function createTypesenseClient() {
+  const config = useRuntimeConfig();
+
+  return new Typesense.Client({
+    nodes: [
+      {
+        host: config.public.typesenseHost,
+        port: Number(config.public.typesensePort),
+        protocol: config.public.typesenseProtocol,
+      },
+    ],
+    apiKey: config.public.typesenseSearchOnlyKey,
+    connectionTimeoutSeconds: 5,
+    retryIntervalSeconds: 0.1,
+    numRetries: 3,
+  });
+}

a00e96ccebdf88f45e28dcb101eb676310208d2f
Theekshana Bandara <Theekshana.Bandara@intrepidtravel.com>
2026-05-05 10:42:42 +0530

    updated result display logic to be compatible with group by

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,7 +10,9 @@ const { search } = useSearch()
 
 const handleSearch = async () => {
     const res = await search(query.value)
-    results.value = res.hits || []
+    results.value = (res.grouped_hits || []).map((group: any) => {
+        return group.hits[0]
+    })
 }
 </script>
 

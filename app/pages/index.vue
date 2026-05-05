ad8c92934d5271c4eb298142329afb3c709e0d68
Theekshana Bandara <Theekshana.Bandara@intrepidtravel.com>
2026-05-05 09:13:38 +0530

    setup search page

---
+++ app/pages/index.vue
@@ -0,0 +1,29 @@
+<!-- /pages/search.vue -->
+
+<script setup lang="ts">
+import { useSearch } from '../composables/useSearch'
+
+const query = ref('')
+const results = ref<any[]>([])
+
+const { search } = useSearch()
+
+const handleSearch = async () => {
+    const res = await search(query.value)
+    results.value = res.hits || []
+}
+</script>
+
+<template>
+    <div>
+        <h1>Typesense Search POC</h1>
+
+        <input v-model="query" placeholder="Search trips..." />
+        <button @click="handleSearch">Search</button>
+
+        <div v-for="hit in results" :key="hit.document.id">
+            <h3>{{ hit.document.name }}</h3>
+            <p>{{ hit.document.primaryCountry }}</p>
+        </div>
+    </div>
+</template>
